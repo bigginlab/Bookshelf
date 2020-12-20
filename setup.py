2afc6194bc4989f9228a6af0143a75ab8002bfbb
Phil Biggin <51948186+philbiggin@users.noreply.github.com>
2020-12-20 17:30:54 +0000

    setup.py
    added setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+"""
+Bookshelf
+A simple tool to manage trajectories
+"""
+import sys
+from setuptools import setup, find_packages
+import versioneer
+
+short_description = __doc__.split("\n")
+
+
+try:
+    with open("README.md", "r") as handle:
+        long_description = handle.read()
+except:
+    long_description = "\n".join(short_description[2:])
+
+
+setup(
+    # Self-descriptive entries which should always be present
+    name='bookshelf',
+    author='Philip Biggin and Shobana Vohra',
+    author_email='philip.biggin@bioch.ox.ac.uk',
+    description=short_description[0],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license='GPL',
+
+    # Which Python importable modules should be included when your package is installed
+    # Handled automatically by setuptools. Use 'exclude' to prevent some specific
+    # subpackage(s) from being added, if needed
+    packages=find_packages(),
+
+    # Optional include package data to ship with your package
+    # Customize MANIFEST.in if the general case does not suit your needs
+    # Comment out this line to prevent the files from being packaged with your software
+    include_package_data=True,
+
+)
