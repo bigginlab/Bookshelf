72a7ee9e271c71d27934ef181bf493dd0e603129
Phil Biggin <51948186+philbiggin@users.noreply.github.com>
2020-12-20 18:52:25 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,6 @@ release = '2.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark'
     'sphinx.ext.autosummary',
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
